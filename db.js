258e84352f4e1322a749781abdc08dfde35882e9
AssafSardi <assafsardi@gmail.com>
2025-08-03 20:40:28 +0300

    upload server/db file

---
+++ db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_db'
+});
+
+module.exports = pool.promise();
+
