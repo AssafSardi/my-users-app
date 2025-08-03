861f694263db080eade0deadb36f5e62e646f8fa
AssafSardi <assafsardi@gmail.com>
2025-08-03 20:47:33 +0300

    upload /client/src files

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Users from "./components/Users/Users.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management App</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App
